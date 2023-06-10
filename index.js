bed722815abb5fc4ebc0bef12e0b68f0a85cc19b
bobur2006coder <boburdilmurodov702@gmail.com>
2023-06-10 11:44:07 +0500

    fix

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 let h2 = document.querySelector('h2')
 let box = document.querySelector('.for-filter')
-let num =100;
+let num = 100;
+let he = 0;
 
- setInterval(function () {box.style.filter=`blur(${num--}px)`;
- if(num<=-0){
-    num=0;
- }
-  h2.innerHTML=num +" %"}, 25);
+setInterval(function () {
+   box.style.filter = `blur(${num--}px)`;
+   he++
+   if (num <= -0 && he>=101) {
+      num = 0;
+      he=''
+      h2.classList.add("none")
+   }
+   h2.textContent = (he)+"%"
+}, 35);
 
 
 
-   
+
 
